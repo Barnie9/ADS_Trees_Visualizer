30f03353c6a22b166e49e3f3508730f64cbbd0a4
Barnie9 <bran.alexandru.2002@gmail.com>
2023-01-07 13:25:30 +0200

    Interface Demo

--- src/main/java/red_black_tree/user_interface/RedBlackTreeMain.java
+++ src/main/java/red_black_tree/user_interface/RedBlackTreeMain.java
@@ -16,6 +16,7 @@ public class RedBlackTreeMain extends Application {
         stage.setScene(scene);
         stage.show();
         // Test
+        // Test1
     }
 
     public static void main(String[] args) {
